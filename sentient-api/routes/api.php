6aa0babdf28a094359bc5c5b0400f6b79f37d610
unknown <ardlikafimurobby02@gmail.com>
2025-05-27 02:14:31 +0700

    fix: memastikan server Laravel berjalan dan dapat diakses dari aplikasi Flutter

--- sentient-api/routes/api.php
+++ sentient-api/routes/api.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->get('profile', [AuthController::class, 'profile']); 
+Route::middleware('auth:api')->get('profile', [AuthController::class, 'profile']);
+Route::post('profile/avatar', [AuthController::class, 'updateAvatar'])->middleware('auth:api'); 
